4f1774520fa0b350da787cc07a1b7db216262153
Adam Rackis <adamrackis@hotmail.com>
2026-05-09 17:08:21 -0500

    work

---
+++ module-1/lesson-2b-instructor.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import { Artist } from "./utils/lesson-2-utils";
+
+type UpdateArtistPayload = {
+  artistId: string;
+  userId: string;
+  reason: "FIXING_ERROR" | "NEW_AVATAR" | "NEW_NAME";
+  payload: Omit<Artist, "id">;
+};
+
+type UpdateArtistResponse =
+  | {
+      success: true;
+      changes: Partial<Artist>;
+    }
+  | {
+      success: false;
+      error: string;
+    };
+
+export async function updateArtist(payload: UpdateArtistPayload) {
+  const success = Math.random() > 0.5 ? true : false;
+
+  if (!success) {
+    return {
+      success: false,
+      error: "Failed to update artist",
+    };
+  }
+
+  return {
+    success: true,
+    changes: {
+      name: "New Name",
+    },
+  };
+}
