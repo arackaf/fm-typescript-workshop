c7291b78df93079a60211140254045c8cd7c67e1
Adam Rackis <adamrackis@hotmail.com>
2026-05-10 17:59:36 -0500

    work

--- module-3/lesson-1-instructor.ts
+++ module-3/lesson-1-instructor.ts
@@ -1,74 +1,10 @@
-type Cat = {
-  name: string;
-  whiskers: number;
-  feral: boolean;
+let n: number;
 
-  // casing does not matter - I just wanted it to stand out - could name it anything
-  TYPE: "CAT"; // add later
-};
+let s: string;
 
-type Dog = {
-  name: string;
-  obedient: boolean;
+let n_or_s: number | string;
 
-  TYPE: "DOG"; // add later
-};
+type True = true;
+type False = false;
 
-// let animal: Cat | Dog;
-// animal.
-
-function dealWithCatOrDog(animal: Cat | Dog) {
-  if ("whiskers" in animal) {
-    console.log("Cat", animal.name, animal.whiskers);
-  } else {
-    console.log("Dog", animal.name, animal.obedient);
-  }
-}
-
-function dealWithCatOrDog2(animal: Cat | Dog) {
-  if (animal.TYPE === "CAT") {
-    console.log("Cat", animal.name, animal.whiskers);
-  } else {
-    console.log("Dog", animal.name, animal.obedient);
-  }
-}
-
-const randomObject = {
-  name: "Muffin",
-  whiskers: 12,
-
-  TYPE: "CAT" as const, //add the as const after showing the error
-};
-
-let someCat: Cat;
-
-someCat = randomObject;
-
-type StringOrNumber = string | number;
-
-let sOrN: StringOrNumber = Math.random() < 0.5 ? 12 : "Hello";
-
-if (typeof sOrN === "string") {
-  console.log(sOrN.toLowerCase());
-} else {
-  console.log(sOrN.toFixed(2));
-}
-
-class A {
-  a: string = "A";
-}
-
-class B {
-  b: string = "B";
-}
-
-type AorB = A | B;
-let aOrB: AorB = Math.random() < 0.5 ? new A() : new B();
-
-if (aOrB instanceof A) {
-  console.log(aOrB.a);
-} else {
-  console.log(aOrB.b);
-}
-
-export {};
+type TrueOrFalse = True | False;
---
+++ module-3/lesson-2-instructor.ts
@@ -0,0 +1,74 @@
+type Cat = {
+  name: string;
+  whiskers: number;
+  feral: boolean;
+
+  // casing does not matter - I just wanted it to stand out - could name it anything
+  TYPE: "CAT"; // add later
+};
+
+type Dog = {
+  name: string;
+  obedient: boolean;
+
+  TYPE: "DOG"; // add later
+};
+
+// let animal: Cat | Dog;
+// animal.
+
+function dealWithCatOrDog(animal: Cat | Dog) {
+  if ("whiskers" in animal) {
+    console.log("Cat", animal.name, animal.whiskers);
+  } else {
+    console.log("Dog", animal.name, animal.obedient);
+  }
+}
+
+function dealWithCatOrDog2(animal: Cat | Dog) {
+  if (animal.TYPE === "CAT") {
+    console.log("Cat", animal.name, animal.whiskers);
+  } else {
+    console.log("Dog", animal.name, animal.obedient);
+  }
+}
+
+const randomObject = {
+  name: "Muffin",
+  whiskers: 12,
+
+  TYPE: "CAT" as const, //add the as const after showing the error
+};
+
+let someCat: Cat;
+
+someCat = randomObject;
+
+type StringOrNumber = string | number;
+
+let sOrN: StringOrNumber = Math.random() < 0.5 ? 12 : "Hello";
+
+if (typeof sOrN === "string") {
+  console.log(sOrN.toLowerCase());
+} else {
+  console.log(sOrN.toFixed(2));
+}
+
+class A {
+  a: string = "A";
+}
+
+class B {
+  b: string = "B";
+}
+
+type AorB = A | B;
+let aOrB: AorB = Math.random() < 0.5 ? new A() : new B();
+
+if (aOrB instanceof A) {
+  console.log(aOrB.a);
+} else {
+  console.log(aOrB.b);
+}
+
+export {};
