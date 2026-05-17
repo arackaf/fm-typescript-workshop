f581c89daa09de33a37580c0eb67e14c21feb268
Adam Rackis <adamrackis@hotmail.com>
2026-05-17 15:08:49 -0500

    another type guard lesson

---
+++ module-3/lesson-8-instructor.ts
@@ -0,0 +1,16 @@
+const arr = [1, 2, 3, "a", "b"];
+
+arr
+  .filter((x) => x)
+  // can add numbers only (as far as TS is concerned)
+  .map((x) => x + 1);
+
+arr
+  // numbers only!
+  .filter((x): x is number => typeof x === "number")
+  .map((x) => x + 1);
+
+arr
+  // numbers only!
+  .filter((x) => typeof x === "number")
+  .map((x) => x + 1);
