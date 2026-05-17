c95b5bfaab1bb294889e77a01c10fd77eb88bd6c
Adam Rackis <adamrackis@hotmail.com>
2026-05-17 11:04:08 -0500

    work

--- module-6/lesson-1.ts
+++ module-6/lesson-1.ts
@@ -1,1 +1,14 @@
+type YesOrNo = "YES" | "NO";
+
+// --------------
+
+const paths = {
+  users: "/users",
+  userContacts: "/users/contacts",
+  user: "/users/:id",
+  settings: "/admin/settings",
+  billing: "/admin/billing",
+  account: "/admin/account",
+} as const;
+
 export {};
