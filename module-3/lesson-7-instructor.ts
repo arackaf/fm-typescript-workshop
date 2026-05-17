8711d310561d44fc40bfcc9ab51798e1a8ee5779
Adam Rackis <adamrackis@hotmail.com>
2026-05-17 15:05:27 -0500

    Add lesson on type guards

---
+++ module-3/lesson-7-instructor.ts
@@ -0,0 +1,52 @@
+type Cat = {
+  name: string;
+  whiskers: number;
+  weight: number;
+  hunt(distanceToPounce: number): void;
+};
+
+type Lion = {
+  name: string;
+  whiskers: number;
+  weight: number;
+  hunt(speed: number, distance: number): void;
+};
+
+type Dog = {
+  name: string;
+  obedient: boolean;
+};
+
+type Animal = Cat | Dog | Lion;
+
+function dealWithAnimal(animal: Animal) {
+  if (animalIsCat(animal)) {
+    dealWithCat(animal);
+  } else if (animalIsLion(animal)) {
+    dealWithLion(animal);
+  } else {
+    dealWithDog(animal);
+  }
+}
+
+function animalIsLion(animal: Animal): animal is Lion {
+  return /.*scary.*/.test(animal.name);
+}
+
+function animalIsCat(animal: Animal): animal is Cat {
+  return !("obedient" in animal) && !animalIsLion(animal);
+}
+
+function dealWithCat(animal: Cat) {
+  console.log("Cat");
+}
+
+function dealWithDog(animal: Dog) {
+  console.log("Dog");
+}
+
+function dealWithLion(animal: Lion) {
+  console.log("Lion");
+}
+
+export {};
