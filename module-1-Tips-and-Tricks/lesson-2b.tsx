ee448e19dd7812154dfcca5dfc4c9048a0155f20
Adam Rackis <adamrackis@hotmail.com>
2026-05-18 11:46:09 -0500

    cleanup

--- module-1-Tips-and-Tricks/lesson-2b.tsx
+++ module-1-Tips-and-Tricks/lesson-2b.tsx
@@ -1,4 +1,3 @@
-import React, { FC } from "react";
 import { Artist } from "./utils/lesson-2-utils";
 
 type UpdateArtistPayload = {
@@ -7,16 +6,6 @@ type UpdateArtistPayload = {
   reason: "FIXING_ERROR" | "NEW_AVATAR" | "NEW_NAME";
   payload: Omit<Artist, "id">;
 };
-
-type UpdateArtistResponse =
-  | {
-      success: true;
-      changes: Partial<Artist>;
-    }
-  | {
-      success: false;
-      error: string;
-    };
 
 export async function updateArtist(payload: UpdateArtistPayload) {
   const success = !payload.artistId.startsWith("7") ? true : false;
