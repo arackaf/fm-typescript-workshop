99292e1196fb8f9668faf6104a63bd2e77fd9a5a
Adam Rackis <adamrackis@hotmail.com>
2026-05-19 11:49:23 -0500

    cleanup mapped types lesson 1

--- module-9-Mapped-Types/lesson-1.ts
+++ module-9-Mapped-Types/lesson-1.ts
@@ -2,26 +2,7 @@
 
 // Array.map
 
-const a = [1, 2, "3"].map(val => {
-  // @ts-ignore
-  return val + 1;
-});
-
-const b = [1, 2, "3"]
-  .filter(val => typeof val === "number")
-  .map(val => {
-    return val + 1;
-  });
-
 // Prelude
-
-type Union = "A" | "B" | 12 | 42 | "C";
-type OnlyStrings<T> = T extends string ? T : never;
-
-type StringsInUnion = OnlyStrings<Union>;
-
-type Junk = `Valid Values are ${Union}`;
-type JunkStrOnly = `Valid Values are ${StringsInUnion}`;
 
 // Actual Mapped types
 
