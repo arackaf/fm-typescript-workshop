e4f15f50dba7777130bf6664382bd4b0e5934081
Adam Rackis <adamrackis@hotmail.com>
2026-05-21 01:30:12 -0500

    next version

--- module-5-Conditional-Types/ai-exercise-1-prompt-and-result-version-final.tsx
+++ module-5-Conditional-Types/ai-exercise-1-prompt-and-result-version-final.tsx
@@ -1,17 +1,39 @@
+import React, { FC, PropsWithChildren } from "react";
+
+// prompt:
+/*
+Remove Props. Remove PropsForVariant. Rename BaseProps to just Props.
+
+Add href directly to the new BaseProps, conditionally string if V is link, or never if not 
+*/
+
 type Variant = "link" | "button";
 
-type BaseProps<V extends Variant> = {
+type Props<V extends Variant> = {
   variant: V;
   onClick?: () => void;
   className?: string;
   style?: React.CSSProperties;
   ariaLabel?: string;
+  href: V extends "link" ? string : never;
 };
 
-type PropsForVariant<V extends Variant> = V extends "link"
-  ? { href: string }
-  : { href?: never };
+const Button: FC<PropsWithChildren<Props<"link"> | Props<"button">>> = props =>
+  null;
 
-export type Props =
-  | (BaseProps<"link"> & PropsForVariant<"link">)
-  | (BaseProps<"button"> & PropsForVariant<"button">);
+export const Component: FC = () => {
+  return (
+    <div>
+      <Button variant="link" href="/">
+        Link
+      </Button>
+      <Button variant="link" href="/" onClick={() => {}}>
+        Link
+      </Button>
+      {/* @ts-expect-error */}
+      <Button variant="button" href="/">
+        Link
+      </Button>
+    </div>
+  );
+};
