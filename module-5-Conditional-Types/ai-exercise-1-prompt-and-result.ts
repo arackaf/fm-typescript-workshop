fdb094ce0323c812f385318dae036e5e8db4f7cf
Adam Rackis <adamrackis@hotmail.com>
2026-05-21 00:58:51 -0500

    first result

--- module-5-Conditional-Types/ai-exercise-1-prompt-and-result.ts
+++ module-5-Conditional-Types/ai-exercise-1-prompt-and-result.ts
@@ -1,10 +1,17 @@
-export type Props = {
-  variant: "link" | "button";
-  href?: string;
+// prompt:
+
+// make this a conditional type - href should be required if variant is "link" but absent if "button"
+
+// new type produced by composer-2.5-fast
+type BaseProps = {
   onClick?: () => void;
   className?: string;
   style?: React.CSSProperties;
   ariaLabel?: string;
 };
 
-export {};
+type PropsForVariant<V extends "link" | "button"> = BaseProps & {
+  variant: V;
+} & (V extends "link" ? { href: string } : { href?: never });
+
+export type Props = PropsForVariant<"link"> | PropsForVariant<"button">;
