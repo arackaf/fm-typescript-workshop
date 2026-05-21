85556956e1ab7c478ddf3afdb82d7658e0290492
Adam Rackis <adamrackis@hotmail.com>
2026-05-21 03:17:47 -0500

    add bonus file

---
+++ module-5-Conditional-Types/ai-exercise-1-prompt-and-result-version-4-manual-bonus.tsx
@@ -0,0 +1,44 @@
+import React, { FC, PropsWithChildren } from "react";
+
+type Variant = "link" | "button";
+
+type BaseProps<V extends Variant> = {
+  variant: V;
+  onClick?: () => void;
+  className?: string;
+  style?: React.CSSProperties;
+  ariaLabel?: string;
+};
+
+type PropsForVariant<V extends Variant> = V extends "link"
+  ? { href: string }
+  : { href?: never };
+
+export type Props<T extends Variant> = BaseProps<T> & PropsForVariant<T>;
+
+function Button<T extends Variant>(
+  props: PropsWithChildren<Props<T>>
+): ReturnType<FC> {
+  return <div>{/* TODO */}</div>;
+}
+
+export const Component: FC = () => {
+  const badProps = { variant: "button" as const, href: "/" };
+  return (
+    <div>
+      <Button {...badProps}>Link</Button>
+      <Button variant="link" href="/">
+        Link
+      </Button>
+      <Button variant="link" href="/" onClick={() => {}}>
+        Link
+      </Button>
+      {/* @ts-expect-error */}
+      <Button variant="link">Link</Button>
+      {/* @ts-expect-error */}
+      <Button variant="button" href="/">
+        Link
+      </Button>
+    </div>
+  );
+};
